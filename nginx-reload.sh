b84b13d786c8adcf1d579cc8ec0dec81f6b610ae
Alessandro Capra <alessandrocapra@gmail.com>
2026-05-21 18:11:46 +0200

    fix: rimuove -it da nginx-reload.sh
    Il flag -it fa fallire 'docker exec' quando auto-deploy.sh gira da cron
    (nessun TTY): il reload di nginx veniva saltato silenziosamente e le
    modifiche alle config non venivano applicate.

--- nginx-reload.sh
+++ nginx-reload.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker exec -it dhunica_proxypass  nginx -s reload
+# Niente flag -it: l'auto-deploy gira da cron senza TTY e "-t" lo farebbe
+# fallire con "the input device is not a TTY", saltando il reload di nginx.
+docker exec dhunica_proxypass nginx -s reload
